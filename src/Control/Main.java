06f3d9fb5c191661e01ba3e4296bdfee6e715207
Felipe Alarcon <felipealarcon264@gmail.com>
2025-10-13 13:14:17 +0000

    Commit desde la tablet

--- src/Control/Main.java
+++ src/Control/Main.java
@@ -1,7 +1,7 @@
 package Control;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Commit desde la tablet!");
     }
 
 }
